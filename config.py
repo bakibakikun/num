0673207d6cffaee0d2615e6114045fc1507d742b
damn <113886837+bakibakikun@users.noreply.github.com>
2025-05-21 18:44:43 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+def fetch_bot_settings():
+    return {
+        "bot2": {
+            "TOKEN": "8173622705:AAE88BPX5k1mHuwFFBlWJS8ixxa36EmuCC0",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002609563244,
+            "PRICE": 3.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} RUB 🇷🇺\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "TON_MERCHANT_ID": "your_ton_merchant_id_here",
+            "TON_API_TOKEN": "your_ton_api_token_here"
+        }
+    }
